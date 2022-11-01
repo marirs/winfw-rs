86ef8bebf4467e091e3290381fdc6736caf26c2d
Dilawar Singh <dilawar@subcom.tech>
2022-11-01 15:17:10 +0530

    fix: Builds with visual studio 2022

--- build.rs
+++ build.rs
@@ -3,13 +3,30 @@
 // <LICENSE-MIT or http://opensource.org/licenses/MIT>
 // All files in the project carrying such notice may not be copied, modified, or distributed
 // except according to those terms.
-extern crate cc;
+
+use cc;
 
 #[cfg(not(windows))]
-fn main() {}
+fn main() {
+    eprintln!("It is a Windows only crate.");
+}
 
 #[cfg(windows)]
 fn main() {
+    // Search of altcomcli.h file if possible.
+    let mut incpath = String::new();
+    for entry in glob::glob("C:/Program Files/Microsoft Visual Studio/**/atlcomcli.h")
+        .expect("Failed to read glob pattern")
+    {
+        match entry {
+            Ok(p) => {
+                println!("Found {:?}", p);
+                incpath.push_str(&format!(";{}", p.parent().unwrap().display()));
+            }
+            Err(e) => println!("{:?}", e),
+        }
+    }
+    std::env::set_var("INCLUDE", incpath);
     cc::Build::new()
         .cpp(true)
         .file("src/bindings.cpp")
